13311185677626ed0982c5739fcab3639835015a
Ashwanth2310 <ashwanth@rajakumar.net>
2024-07-20 10:29:51 +0530

    Added the custom font to the header of app

--- App.tsx
+++ App.tsx
@@ -1,73 +1,77 @@
-import * as React from 'react'
-
+import * as React from 'react';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import * as FileSystem from 'expo-file-system'
-import {Asset} from 'expo-asset'
-import {SQLiteProvider} from 'expo-sqlite/next'
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as FileSystem from 'expo-file-system';
+import { Asset } from 'expo-asset';
+import { SQLiteProvider } from 'expo-sqlite/next';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './screens/Home';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-const Stack = createNativeStackNavigator()
-
-const loadDatabase = async () => {
-  const dbName = "mySQLiteDB.db";
-  const dbAsset = require("./assets/mySQLiteDB.db");
-  const dbUri = Asset.fromModule(dbAsset).uri;
-  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
-
-  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
-  if (!fileInfo.exists) {
-    await FileSystem.makeDirectoryAsync(
-      `${FileSystem.documentDirectory}SQLite`,
-      { intermediates: true }
-    );
-    await FileSystem.downloadAsync(dbUri, dbFilePath);
-  }
-};
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [DbLoaded,setDbLoaaded] =  React.useState<boolean>(false);
+  const [DbLoaded, setDbLoaded] = React.useState<boolean>(false);
+  const [fontsLoaded] = useFonts({
+    Nothing: require('./assets/fonts/nothingfont.otf'),
+  });
 
-  React.useEffect(()=>{
-    loadDatabase()
-    .then(()=> setDbLoaaded(true))
-    .catch((e) => console.error(e))
-  })
+  React.useEffect(() => {
+    async function loadDatabase() {
+      const dbName = 'mySQLiteDB.db';
+      const dbAsset = require('./assets/mySQLiteDB.db');
+      const dbUri = Asset.fromModule(dbAsset).uri;
+      const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
 
-  if(!DbLoaded) return(
-    <View style = {{flex:1}}>
-      <ActivityIndicator size = {"large"}/>
-      <Text>Loading....</Text>
-    </View>
-  )
+      const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+      if (!fileInfo.exists) {
+        await FileSystem.makeDirectoryAsync(
+          `${FileSystem.documentDirectory}SQLite`,
+          { intermediates: true }
+        );
+        await FileSystem.downloadAsync(dbUri, dbFilePath);
+      }
+      setDbLoaded(true);
+    }
+
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+      await loadDatabase();
+    }
+
+    prepare().catch((e) => console.error(e));
+  }, []);
+
+  React.useEffect(() => {
+    if (fontsLoaded && DbLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, DbLoaded]);
+
+  if (!fontsLoaded || !DbLoaded)
+    return (
+      <View style={{ flex: 1 }}>
+        <ActivityIndicator size={'large'} />
+        <Text>Loading....</Text>
+      </View>
+    );
 
   return (
     <NavigationContainer>
-    <React.Suspense
-    fallback={
-      <View style = {{flex:1}}>
-      <ActivityIndicator size = {"large"}/>
-      <Text>Loading....</Text>
-    </View>
-    }
-    >
       <SQLiteProvider databaseName='mySQLiteDB.db' useSuspense>
         <Stack.Navigator>
-          <Stack.Screen 
-          name = "Home"
-          component={Home}
-          options={{
-            headerTitle: "💲NOTHING TRACKER ⬛ ",
-            headerLargeTitle: true,
-          }}
+          <Stack.Screen
+            name='Home'
+            component={Home}
+            options={{
+              headerTitle: '💲NOTHING TRACKER ⬛',
+              headerLargeTitle: true,
+              headerTitleStyle: { fontFamily: 'Nothing' }
+            }}
           />
-          
         </Stack.Navigator>
       </SQLiteProvider>
-    </React.Suspense>
     </NavigationContainer>
   );
 }
-
-
